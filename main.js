812694fad2dce34b62106fec536d151e10338538
Cullie <cullie.02@gmail.com>
2021-08-09 15:47:34 +1000

    main.js, gitignore

---
+++ main.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Library of Babel');
+});
+
+//Keep this as last line
+client.login('ODc0MTU5NTIwNTY4NDU1MTg4.YRC6fg.zgZFOFqhysz_epuBQ92c2DYZ54s');
