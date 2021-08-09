9cb6304952426ea9e074cb2c0a1200d6822176b5
Cullie <cullie.02@gmail.com>
2021-08-09 16:09:35 +1000

    simple command

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [] });
 
+const prefix = '-';
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping')
+    {
+        message.channel.send('pong!');
+    }
+
+
+});
+
+// ! DEBUG
 client.once('ready', () => {
     console.log('Library of Babel');
 });
